68aceb7b5adafd9b151fa2ade71db2dfe8b6327c
manny-e1 <aewnetu6@gmail.com>
2023-05-26 10:08:04 +0300

    users and roles schema and migration

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,8 +1,38 @@
-import {pgTable, timestamp, uuid, varchar} from 'drizzle-orm/pg-core'
+import {pgTable, primaryKey, text, timestamp, uniqueIndex, uuid, varchar} from 'drizzle-orm/pg-core'
 
 export const applications = pgTable("applications",{
     id: uuid("id").primaryKey().defaultRandom(),
     name: varchar("name",{length:256}).notNull(),
     createdAt: timestamp("created_at").defaultNow().notNull(),
     updatedAt: timestamp("updated_at").defaultNow().notNull(),
+})
+
+export const users = pgTable('users', {
+    id: uuid("id").defaultRandom().notNull(),
+    email: varchar('email', {length:256}).notNull(),
+    applicationId: uuid('applicationId').references(()=>applications.id),
+    password: varchar("password", {length:256}).notNull(),
+    permissions: text('permissions').array().$type<Array<string>>(),
+   createdAt: timestamp("created_at").defaultNow().notNull(),
+   updatedAt: timestamp("updated_at").defaultNow().notNull()
+},(users)=>{
+    return {
+        cpk: primaryKey(users.email, users.applicationId),
+        idIndex: uniqueIndex("users_id_index").on(users.id),
+    }
+})
+
+
+export const roles = pgTable('roles', {
+    id: uuid("id").defaultRandom().notNull(),
+    name: varchar('name', {length:256}).notNull(),
+    applicationId: uuid('applicationId').references(()=>applications.id),
+    permissions: text('permissions').array().$type<Array<string>>(),
+   createdAt: timestamp("created_at").defaultNow().notNull(),
+   updatedAt: timestamp("updated_at").defaultNow().notNull()
+},(roles)=>{
+    return {
+        cpk: primaryKey(roles.name, roles.applicationId),
+        idIndex: uniqueIndex("roles_id_index").on(roles.id),
+    }
 })
